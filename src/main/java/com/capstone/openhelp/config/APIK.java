a4cbb42ac5000ef49153a56c0cd91dc6817249e8
ChristianCrousser <christian.crousserkaiman@gmail.com>
2020-02-12 14:58:40 -0600

    changed file

--- src/main/java/com/capstone/openhelp/config/APIK.java
+++ src/main/java/com/capstone/openhelp/config/APIK.java
@@ -5,7 +5,7 @@ import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
 public class APIK {
-    public static final String API_KEY = "61fee86fa2216cf329d49c6122f07367-52b6835e-c057b1e0";
+    public static final String API_KEY = "";
 
     public static void main(String[] args) {
 
