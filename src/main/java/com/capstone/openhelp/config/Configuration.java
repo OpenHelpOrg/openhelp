4b6cd4be78f9db6fea9564ca481e1c003e4a27a5
ChristianCrousser <christian.crousserkaiman@gmail.com>
2020-02-12 13:57:57 -0600

    about css changed

--- src/main/java/com/capstone/openhelp/config/Configuration.java
+++ src/main/java/com/capstone/openhelp/config/Configuration.java
@@ -9,17 +9,12 @@ import static com.capstone.openhelp.config.Domain.YOUR_DOMAIN_NAME;
 
 public class Configuration {
 
-//    Configuration configuration = new Configuration()
-//            .domain("myopenhelp.com")
-//            .apiKey("key-52b6835e-c057b1e0")
-
-//            .from("Test account", "test@myopenhelp.com");
     public static JsonNode sendSimpleMessage() throws UnirestException {
 
         HttpResponse<com.mashape.unirest.http.JsonNode> request = Unirest.post("https://api.mailgun.net/v3/" + YOUR_DOMAIN_NAME + "/messages")
                 .basicAuth("api", APIK.API_KEY)
                 .field("from", "Excited User <test@openhelp.com>")
-                .field("to", "walsh.c.bryan@gmail.com")
+                .field("to", "bryan.c.walsh@gmail.com")
                 .field("subject", "hello, and have a nice day!")
                 .field("text", "testing")
                 .asJson();
