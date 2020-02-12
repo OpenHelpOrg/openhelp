cd8d23e806101e79ef593ab73a88b0aa4aa3e138
ChristianCrousser <christian.crousserkaiman@gmail.com>
2020-02-12 13:23:54 -0600

    added email

---
+++ src/main/java/com/capstone/openhelp/config/Domain.java
@@ -0,0 +1,13 @@
+package com.capstone.openhelp.config;
+
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+public class Domain {
+    public static final String YOUR_DOMAIN_NAME = "myopenhelp.com";
+
+    public static void main(String[] args) {
+
+    }
+}
