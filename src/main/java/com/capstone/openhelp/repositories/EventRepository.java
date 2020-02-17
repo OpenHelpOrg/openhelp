52b8599c5593a4ff99f4ac2bf9c9bf56c49d420a
Roger Mayer <rmayer1984@gmail.com>
2020-02-16 20:59:40 -0600

    search

--- src/main/java/com/capstone/openhelp/repositories/EventRepository.java
+++ src/main/java/com/capstone/openhelp/repositories/EventRepository.java
@@ -3,8 +3,11 @@ package com.capstone.openhelp.repositories;
 import com.capstone.openhelp.models.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface EventRepository extends JpaRepository<Event, Long> {
     Event findById(long id);
     Event deleteById(long id);
+    List<Event>  findByTitleContainsOrSummaryContainsAllIgnoreCase(String title, String summary);
 }
