51169e5053e8075546a920f06ba32f7ccf6cfdf6
Eduardo Valerio Gamez <evaleriog@outlook.com>
2020-02-16 19:03:19 -0600

    Adding email functionality on register

--- src/main/java/com/capstone/openhelp/controllers/LoginController.java
+++ src/main/java/com/capstone/openhelp/controllers/LoginController.java
@@ -10,45 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class LoginController {
     private String username;
 
-//    @GetMapping("/login")
-//    public String showLogin(){
-//        return "login";
-//    }
-
     @GetMapping("/login")
     public String showLogin(Model model){
-        model.addAttribute("user", new User());
+        //model.addAttribute("user", new User());
         return "login";
     }
 
-//    @PostMapping("/login")
-//    public String Login(){
-//        return "redirect:/index";
-//    }
-
-//    @PostMapping("/login")
-//    public String authLogin(
-//            @RequestParam String username,
-//            @RequestParam String password,
-//            Model model
-//    ) {
-//        System.out.println(username);
-//        System.out.println(password);
-//        if(username.equals("admin"))
-//            if(password.equals("password")) {
-//                this.username = username;
-//                System.out.println("yes");
-//                model.addAttribute("username", username);
-//                return "profile";
-//            }
-//        model.addAttribute("alert", true);
-//        return "users/login";
-//
-//    }
-//
-//    @GetMapping("/profile")
-//    public String showProfile(Model model){
-//        model.addAttribute("username", username);
-//        return "profile";
-//    }
 }
