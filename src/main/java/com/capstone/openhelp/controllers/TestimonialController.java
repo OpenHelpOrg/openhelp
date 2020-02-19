98063ff8db10f2a7f4b320a104e4f9855c1e6a66
ChristianCrousser <christian.crousserkaiman@gmail.com>
2020-02-19 11:13:43 -0600

    added to testimonial controller

--- src/main/java/com/capstone/openhelp/controllers/TestimonialController.java
+++ src/main/java/com/capstone/openhelp/controllers/TestimonialController.java
@@ -1,7 +1,9 @@
 package com.capstone.openhelp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
     @Controller
@@ -9,7 +11,8 @@ public class TestimonialController {
 
         @GetMapping("/testimonials")
         @ResponseBody
-        public String testimonialPage() {
+        public String testimonials(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+            model.addAttribute("name", name);
             return "testimonials";
         }
 
